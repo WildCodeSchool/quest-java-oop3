d138650cb2fff458b245e08f56bdb480e7da9237
v3ry <v3ryfr@gmail.com>
2022-11-07 15:52:43 +0100

    not the best way but it work

--- Eagle.java
+++ Eagle.java
@@ -1,12 +1,14 @@
-public class Eagle extends Bird {
+public class Eagle extends Bird implements Fly {
 
     private boolean flying;
     private int altitude;
+    private String name;
 
     public Eagle(String name) {
         super(name);
         this.flying = false;
         this.altitude = 0;
+        this.name = name;
     }
 
     public int getAltitude() {
@@ -20,5 +22,36 @@ public class Eagle extends Bird {
     @Override
     public String sing() {
         return "Screech!";
+    }
+
+    @Override
+    public String takeOff() {
+        System.out.println(name + " takes off in the sky");
+        return name + " takes off in the sky";
+    }
+
+    @Override
+    public String ascend(int meters) {
+        System.out.println(name + " flies upward, altitude : " + meters + "m");
+        return name + " flies upward, altitude : " + meters + "m";
+    }
+
+    @Override
+    public String descend(int meters) {
+        System.out.println(name + " flies downward, altitude : " + meters + "m");
+        return name + " flies downward, altitude : " + meters + "m";
+    }
+
+    @Override
+    public String glide() {
+        System.out.println("It glides into the air.");
+        return "It glides into the air.";
+    }
+
+    @Override
+    public String land() {
+        System.out.println(name + " lands on the ground.");
+        return name + " lands on the ground.";
+        
     }
 }
---
+++ Fly.java
@@ -0,0 +1,11 @@
+public interface Fly  {
+    String takeOff();
+
+    String ascend(int meters);
+
+    String descend(int meters);
+
+    String glide();
+
+    String land();
+}
